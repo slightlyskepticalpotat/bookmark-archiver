46290c89fc239556defa07f2f04054ee99161955
Anthony Chen <chenanthony365@gmail.com>
2022-01-21 18:15:16 -0500

    basic improvements

--- README.md
+++ README.md
@@ -1,15 +1,41 @@
 # bookmarkarchiver
-Python script that archives all of your bookmarks on the Wayback Machine. Supports all major browsers.
+Python script that archives all of your bookmarks on the Internet Archive. Supports all major browsers.
+
+bookmarkarchiver uses the official Save Page Now [API](https://docs.google.com/document/d/1Nsv52MvSjbLb2PCpHlat0gkzw0EvtSgpKHu4mk0MnrA). Anonymous users are limited to 4,000 requests per day, which should be enough to save around 200 websites. If you create a free account to the [Internet Archive](https://archive.org/) and log in with Chrome, Chromium, or Firefox, your single-day request limit increases to 100,000 and you should be able to save approximately 5000 websites.
+
+As for dependencies, bookmarkarchiver uses Richard Penman's [browsercookie](https://github.com/richardpenman/browsercookie) module with [this patch](https://github.com/richardpenman/browsercookie/pull/8/commits/badb6e122d8ff24b3494babf74e1a4dad1538f8e) applied. It also uses the Python [requests](https://docs.python-requests.org/en/latest/) library.
+
+## Usage
+To use bookmarkarchiver, you need a bookmark file. You can get one by exporting them from a browser—instructions are online.
+```
+$ pip3 -r requirements.txt
+$ python3 bookmarkarchiver.py --help
+usage: bookmarkarchiver.py [-h] [--capture_all] [--capture_outlinks] [--capture_screenshot] [--delay_wb_availability] [--force_get]
+                           [--skip_first_archive] [--email_result]
+                           bookmark_file
+
+Archives your bookmarks with the Wayback Machine.
+
+positional arguments:
+  bookmark_file         A Netscape format bookmarks file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --capture_all, -a     Don't capture error pages
+  --capture_outlinks, -o
+                        Capture all outlinks
+  --capture_screenshot, -s
+                        Capture a screenshot
+  --delay_wb_availability, -d
+                        Delay uploading capture
+  --force_get, -g       Force a GET request
+  --skip_first_archive, -f
+                        Don't find old captures
+  --email_result, -e    Email results to user
+```
 
 ## To-Do
-- explain dependency situation
-- add instructions
 - publish as a pip package
-- advertise because this useful
-- lint and fix the file
-- test on other browsers
-- exponential backoff
-- more flags to select
-- summary of sucesses
+- summary of capture status
 - archive to other archivers
-- use friendly name in log
+- track down mysterious crashes
--- bookmarkarchiver.py
+++ bookmarkarchiver.py
@@ -3,22 +3,48 @@ import re
 import secrets
 import time
 
-import browsercookie
 import requests
+import browsercookie
 
+wait_time = 1
+
+# parse and validate command-line arguments
 parser = argparse.ArgumentParser(description="Archives your bookmarks with the Wayback Machine.")
 parser.add_argument("bookmark_file", help="A Netscape format bookmarks file", type=str)
-arguments = parser.parse_args()
-with open(arguments.bookmark_file, "r") as file:
+parser.add_argument("--capture_all", "-a", default=True, action="store_false", help="Don't capture error pages")
+parser.add_argument("--capture_outlinks", "-o", default=False, action="store_true", help="Capture all outlinks")
+parser.add_argument("--capture_screenshot", "-s", default=False, action="store_true", help="Capture a screenshot")
+parser.add_argument("--delay_wb_availability", "-d", default=False, action="store_true", help="Delay uploading capture")
+parser.add_argument("--force_get", "-g", default=False, action="store_true", help="Force a GET request")
+parser.add_argument("--skip_first_archive", "-f", default=True, action="store_false", help="Don't find old captures")
+parser.add_argument("--email_result", "-e", default=False, action="store_true", help="Email results to user")
+arguments = vars(parser.parse_args())
+data = {"bookmark_file": arguments["bookmark_file"]}
+for key in arguments.keys():
+    if arguments[key] is True:
+        data[key] = str(int(arguments[key]))
+print("Setup: command-line arguments loaded")
+
+# parse netscape-format bookmark file
+with open(data['bookmark_file'], "r", encoding="utf-8") as file:
     bookmark_file = file.read()
 bookmark_urls = re.findall(r'(?:HREF=\")(.+?)(?:\")', bookmark_file)
-cookies = browsercookie.load()
+bookmark_names = re.findall(r'(?:\">)(.+?)(?:</A>)', bookmark_file)
+bookmarks = [{"name": bookmark_names[i], "url": bookmark_urls[i]} for i in range(len(bookmark_urls))]
+print(f"Setup: {len(bookmarks)} bookmarks loaded")
 
-for bookmark_url in bookmark_urls:
-    status = requests.get("https://web.archive.org/save/status/user", cookies=cookies, data={"_t": secrets.token_urlsafe(16)}).json()
+# load cookies from chrome or firefox
+cookies = browsercookie.load()
+print("Setup: chrome/firefox cookies loaded")
+
+for bookmark in bookmarks:
+    status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies).json()
     while status["available"] == 0:
-        print("API Limit: sleeping for 120 seconds")
-        time.sleep(120)
-        status = requests.get("https://web.archive.org/save/status/user", cookies=cookies, data={"_t": secrets.token_urlsafe(16)}).json()
-    print(f"Capturing: {bookmark_url}")
-    capture = requests.post(f"https://web.archive.org/save", cookies=cookies, data={"url": bookmark_url, "capture_all": "1", "capture_outlinks": "0", "capture_screenshot": "0", "delay_wb_availability": "0", "skip_first_archive": "1", "if_not_archived_within": "0", "email_result": "0"})
+        print(f"API Limit: sleeping for {wait_time} seconds")
+        time.sleep(wait_time)
+        wait_time *= 2
+        status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies).json()
+    wait_time = 1
+    print(f"Capturing: {bookmark['name']}")
+    data["url"] = bookmark["url"]
+    requests.post("https://web.archive.org/save", data=data, cookies=cookies)
