902da51db86ae852e4fd824c5f48b6f76df48ca9
Anthony Chen <chenanthony365@gmail.com>
2022-01-22 15:35:03 -0500

    add >99% working prototype

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # bookmarkarchiver
 Python script that archives all of your bookmarks on the Internet Archive. Supports all major browsers.
 
-bookmarkarchiver uses the official Save Page Now [API](https://docs.google.com/document/d/1Nsv52MvSjbLb2PCpHlat0gkzw0EvtSgpKHu4mk0MnrA). Anonymous users are limited to 4,000 requests per day, which should be enough to save around 200 websites. If you create a free account to the [Internet Archive](https://archive.org/) and log in with Chrome, Chromium, or Firefox, your single-day request limit increases to 100,000 and you should be able to save approximately 5000 websites.
+bookmarkarchiver uses the official Save Page Now [API](https://docs.google.com/document/d/1Nsv52MvSjbLb2PCpHlat0gkzw0EvtSgpKHu4mk0MnrA). Anonymous users are limited to 4,000 requests per day, which should be enough to save around 200 websites. If you create a free account to the [Internet Archive](https://archive.org/) and log in with Chrome, Chromium, or Firefox, your single-day request limit increases to 100,000 and you should be able to save approximately 5000 websites. Due to API limitations, each bookmark should take around 10 seconds to archive.
 
 As for dependencies, bookmarkarchiver uses Richard Penman's [browsercookie](https://github.com/richardpenman/browsercookie) module with [this patch](https://github.com/richardpenman/browsercookie/pull/8/commits/badb6e122d8ff24b3494babf74e1a4dad1538f8e) applied. It also uses the Python [requests](https://docs.python-requests.org/en/latest/) library.
 
 ## Usage
 To use bookmarkarchiver, you need a bookmark file. You can get one by exporting them from a browser—instructions are online.
 ```
-$ pip3 -r requirements.txt
+$ pip3 install -r requirements.txt
 $ python3 bookmarkarchiver.py --help
-usage: bookmarkarchiver.py [-h] [--capture_all] [--capture_outlinks] [--capture_screenshot] [--delay_wb_availability] [--force_get]
-                           [--skip_first_archive] [--email_result] [--quit_immediately]
+usage: bookmarkarchiver.py [-h] [--no_capture_all] [--capture_outlinks] [--capture_screenshot] [--delay_wb_availability] [--force_get]
+                           [--no_skip_first_archive] [--email_result] [--quit_immediately]
                            bookmark_file
 
 Archives your bookmarks with the Wayback Machine.
@@ -21,16 +21,16 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
-  --capture_all, -a     Don't capture error pages
-  --capture_outlinks, -o
+  --no_capture_all, -n  Don't capture error pages
+  --capture_outlinks, -c
                         Capture all outlinks
   --capture_screenshot, -s
                         Capture a screenshot
   --delay_wb_availability, -d
                         Delay uploading capture
-  --force_get, -g       Force a GET request
-  --skip_first_archive, -f
-                        Don't find old captures
+  --force_get, -f       Force a GET request
+  --no_skip_first_archive, -a
+                        Don't recapture pages
   --email_result, -e    Email results to user
   --quit_immediately, -q
                         Don't show end results
@@ -38,3 +38,6 @@ optional arguments:
 
 ## To-Do
 - publish as a pip package
+- add retrying for errors
+- show which captures errored
+- cli argument of api delay
--- bookmarkarchiver.py
+++ bookmarkarchiver.py
@@ -7,25 +7,27 @@ import requests
 
 import browsercookie
 
-def wait_for_api(n):
-    """Wait until there are n available concurrent requests."""
+JSON_HEADER = {"Accept":  "application/json"}
+
+def wait_for_api(available):
+    """Wait until there are a number of free concurrent requests."""
     wait_time = 1
-    status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies, headers={"Accept": "application/json"}).json()
-    while status["available"] < n:
+    status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies, headers=JSON_HEADER).json()
+    while status["available"] < available:
         print(f"API Limit: sleeping for {wait_time} seconds")
         time.sleep(wait_time)
         wait_time *= 2
-        status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies, headers={"Accept": "application/json"}).json()
+        status = requests.get("https://web.archive.org/save/status/user", data={"_t": secrets.token_urlsafe(16)}, cookies=cookies, headers=JSON_HEADER).json()
 
 # parse and validate command-line arguments
 parser = argparse.ArgumentParser(description="Archives your bookmarks with the Wayback Machine.")
 parser.add_argument("bookmark_file", help="A Netscape format bookmarks file", type=str)
-parser.add_argument("--capture_all", "-a", default=True, action="store_false", help="Don't capture error pages")
-parser.add_argument("--capture_outlinks", "-o", default=False, action="store_true", help="Capture all outlinks")
+parser.add_argument("--no_capture_all", "-n", default=True, action="store_false", help="Don't capture error pages")
+parser.add_argument("--capture_outlinks", "-c", default=False, action="store_true", help="Capture all outlinks")
 parser.add_argument("--capture_screenshot", "-s", default=False, action="store_true", help="Capture a screenshot")
 parser.add_argument("--delay_wb_availability", "-d", default=False, action="store_true", help="Delay uploading capture")
-parser.add_argument("--force_get", "-g", default=False, action="store_true", help="Force a GET request")
-parser.add_argument("--skip_first_archive", "-f", default=True, action="store_false", help="Don't recapture pages")
+parser.add_argument("--force_get", "-f", default=False, action="store_true", help="Force a GET request")
+parser.add_argument("--no_skip_first_archive", "-a", default=True, action="store_false", help="Don't recapture pages")
 parser.add_argument("--email_result", "-e", default=False, action="store_true", help="Email results to user")
 parser.add_argument("--quit_immediately", "-q", default=False, action="store_true", help="Don't show end results")
 arguments = vars(parser.parse_args())
@@ -51,9 +53,10 @@ print("Setting Up: chrome/firefox cookies loaded")
 for bookmark in bookmarks:
     try:
         wait_for_api(1)
+        time.sleep(8) # needed because api lags
         print(f"Capturing: {bookmark['name']}")
         data["url"] = bookmark["url"]
-        request = requests.post("https://web.archive.org/save", data=data, cookies=cookies, headers={"Accept": "application/json"}).json()
+        request = requests.post("https://web.archive.org/save", data=data, cookies=cookies, headers=JSON_HEADER).json()
         bookmark["job_id"] = request["job_id"]
     except KeyError as error:
         print(f"Error: {request['status_ext']}")
@@ -69,6 +72,8 @@ if arguments["quit_immediately"]:
 print("Finishing: waiting for completion")
 wait_for_api(6)
 job_ids = [bookmark.get("job_id", "placeholder-job-id") for bookmark in bookmarks]
-job_ids = [requests.get(f"https://web.archive.org/save/status/{job_id}", cookies=cookies, headers={"Accept": "application/json"}).json()["status"] for job_id in job_ids]
+job_ids = [requests.get(f"https://web.archive.org/save/status/{job_id}", cookies=cookies, headers=JSON_HEADER).json()["status"] for job_id in job_ids]
 print(f"Information: {len(job_ids)} bookmarks in total")
 print(f"Information: {job_ids.count('success')} bookmarks archived")
+if len(job_ids) - job_ids.count('success'):
+    print(f"Information: {len(job_ids) - job_ids.count('success')} failed; check logs")
